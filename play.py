773a7866a36fef191602a0ef94c8bcd8ba12c8d4
Jan <jan.rosendahl@posteo.de>
2016-07-19 08:29:28 -0700

    network connections work now

--- play.py
+++ play.py
@@ -3,36 +3,39 @@
 
 import population as pop
 import tcp as tcp
-
-#tcp.TCPConnection.send([0.312,0.45])
-
-#create network socket to communicate with Starcraft
+import time
 tcp_connect = tcp.TCPConnection()
 
-
-m = tcp_connect.read()
-print(m)
-
-#create base population
 p = pop.Population(100, network_shape=[3,2,3,4])
+while True:
+	#create network socket to communicate with Starcraft
+	tcp_connect.accept()
 
-p.evaulate_network(0,[1,2,3])
-p.set_score(0,12)
-p.evaulate_network(55,[1,2,3])
+	while tcp_connect.is_connected():
+		m = tcp_connect.read()
+		tcp_connect.send([1,1])
+	print('Round over')
+	#time.sleep(1)
+	#create base population
+	
 
-#for number of generations:
-#    for i in range(p.pop_size):
-        # play games and evaluate
+	#p.evaulate_network(0,[1,2,3])
+	#p.set_score(0,12)
+	#p.evaulate_network(55,[1,2,3])
 
-        #while playing
-            #receive current state
-            # input = ....
+	#for number of generations:
+	#    for i in range(p.pop_size):
+		# play games and evaluate
 
-            #and evaluate
-            #pop.evaluate(i,input)
-        # transmit score to population class
-        # pop.set_score(i,new_score)
+		#while playing
+		    #receive current state
+		    # input = ....
 
-#    pop.breed()
+		    #and evaluate
+		    #pop.evaluate(i,input)
+		# transmit score to population class
+		# pop.set_score(i,new_score)
+
+	#    pop.breed()
 
 print("Completed")
--- tcp.py
+++ tcp.py
@@ -13,46 +13,51 @@ class TCPConnection:
         self.sock.bind(server_address)
 
         #Calling listen() puts the socket into server mode...
-        self.sock.listen(1)
-        #and accept() waits for an incoming connection.
+        self.sock.listen(2)
 
+    def accept(self):
         # Wait for a connection
         print >>sys.stderr, 'waiting for a connection'
         self.connection, self.client_address = self.sock.accept()
-
+	self.connected = True
         print("Connected to " + str(self.client_address))
 
     def read(self):
         #read header
         header = self.connection.recv(8)
-        print(header)
+	if not header:
+		self.connected=False
+		return [-1]
+	if len(header) != 8:
+		raise Exception("Got header of invalid size.")
+
+        #print(header)
         magic_number = struct.unpack('B',header[0])[0]
         if magic_number != 37:
             raise Exception("Error while receiving data: Magic Number is wrong. Is " + str(magic_number) +" but should be 37.")
-        #length = data[1]*256 + data[2]
-        message_length = struct.unpack('H',header[1:3])[0]
-        print("message length" + str(message_length))
+        
+        message_length = struct.unpack('>H',header[1:3])[0]
+        #print("message length" + str(message_length))
 
         data_as_string = self.connection.recv(message_length)
+	if not data_as_string:
+		self.connected=False
+		return [-1]
         nr_of_doubles = message_length/8
-        #data_as_list = [0]*nr_of_double
 
-        #for i in range(nr_of_double):
-        ##    data_as_list[i] = struct.unpack
         return struct.unpack('d'*nr_of_doubles, data_as_string)
 
-    @staticmethod
-    def send(message):
-#    def send(self,message):
-        header = [0]*8
-        header[0] = 37
+    def send(self,message):
         message_length = len(message)*8
-        header[1] = int(message_length/256)
-        header[2] = message_length % 256
+	
+	header = struct.pack('>BH5x', 37, message_length)
 
-        #self.connection.send(header)
-        for number in message:
-            data=[0]*8
-            struct.pack_into('s', data,0,number)
-            #self.connection.send(data)
-            print(data)
+        self.connection.send(header)
+        data = ''
+	
+	for number in message:
+		data += struct.pack('d',number)
+	self.connection.send(data)
+
+    def is_connected(self):
+	return self.connected
